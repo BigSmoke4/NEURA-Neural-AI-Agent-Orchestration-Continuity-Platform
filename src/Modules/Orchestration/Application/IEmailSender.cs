6e5e3bd5a484f11c71534bbd0e2cfac154bcbdc1
Ahanaf Mokammel <ahanafmo@gmail.com>
2026-09-02 12:57:38 +0600

    o

--- src/Modules/Orchestration/Application/IEmailSender.cs
+++
@@ -1,7 +0,0 @@
-namespace Neura.Modules.Orchestration.Application;
-
-/// <summary>Section 79: optional email delivery channel for notifications.</summary>
-public interface IEmailSender
-{
-    Task SendAsync(string toAddress, string subject, string body, CancellationToken ct = default);
-}
